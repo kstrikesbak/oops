96ab70f4e5c89f99eece44a990bd592074e637f7
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-11-27 13:08:58 -0500

    stack complete

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const Stack = function() {
+  return {
+    items : [],
+    add : function(str){
+      return this.items.push(str);
+    },
+    remove : function() {
+      return this.items.pop();
+    },
+    peek : function() {
+      return this.items[this.items.length -1]
+    }
+  }
 }
+
 
 const Queue = function() {
 }
